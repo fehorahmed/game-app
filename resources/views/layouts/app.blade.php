62f785fb10a7db32b964c23bd64c48bf55784adf
ahmedfehor <ahmedfehor@gmail.com>
2024-04-01 11:43:54 +0600

    ok

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<!-- Mirrored from mannatthemes.com/metrica/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Feb 2023 15:08:28 GMT -->
+
+<head>
+
+
+    <meta charset="utf-8" />
+    <title>Metrica - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body id="body">
+    <!-- leftbar-tab-menu -->
+    <div class="leftbar-tab-menu">
+        @include('layouts.left-menu')
+    </div>
+    <!-- end leftbar-tab-menu-->
+    <!-- Top Bar Start -->
+    <div class="topbar">
+        <!-- Navbar -->
+        @include('layouts.top-bar')
+        <!-- end navbar-->
+    </div>
+    <!-- Top Bar End -->
+
+    <div class="page-wrapper">
+
+        <!-- Page Content-->
+        <div class="page-content-tab">
+
+            <div class="container-fluid">
+                <!-- Page-Title -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="page-title-box">
+                            <div class="float-end">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Metrica</a>
+                                    </li><!--end nav-item-->
+                                    <li class="breadcrumb-item"><a href="#">Dashboard</a>
+                                    </li><!--end nav-item-->
+                                    <li class="breadcrumb-item active">Analytics</li>
+                                </ol>
+                            </div>
+                            <h4 class="page-title">Analytics</h4>
+                        </div><!--end page-title-box-->
+                    </div><!--end col-->
+                </div>
+                <!-- end page title end breadcrumb -->
+                <!-- end page title end breadcrumb -->
+                @yield('content')
+
+            </div><!-- container -->
+
+            <!--Start Rightbar-->
+            <!--Start Rightbar/offcanvas-->
+            @include('layouts.right-bar')
+            <!--end Rightbar/offcanvas-->
+            <!--end Rightbar-->
+
+            <!--Start Footer-->
+            <!-- Footer Start -->
+            @include('layouts.footer')
+            <!-- end Footer -->
+            <!--end footer-->
+        </div>
+        <!-- end page content -->
+    </div>
+    <!-- end page-wrapper -->
+
+    <!-- Javascript  -->
+    <!-- vendor js -->
+
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/feather-icons/feather.min.js"></script>
+
+    <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/js/pages/analytics-index.init.js"></script>
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
+
+</body>
+<!--end body-->
+
+<!-- Mirrored from mannatthemes.com/metrica/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Feb 2023 15:09:08 GMT -->
+
+</html>
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+
+<!-- Mirrored from mannatthemes.com/metrica/default/auth-login-alt.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Feb 2023 15:10:12 GMT -->
+
+<head>
+
+
+    <meta charset="utf-8" />
+    <title>Metrica - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body id="body" class="auth-page card-bg">
+    <!-- Log In page -->
+    @yield('content')
+    <!-- vendor js -->
+
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/feather-icons/feather.min.js"></script>
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
+
+</body>
+
+
+<!-- Mirrored from mannatthemes.com/metrica/default/auth-login-alt.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Feb 2023 15:10:12 GMT -->
+
+</html>
