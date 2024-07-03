2f4dfc8a0d0fa32ab727618a70c07c1b3d7d2328
fehor-sebpo <fehor.ahmed@sebpo.com>
2024-07-03 21:53:30 +0600

    ok

---
+++ app/Modules/Website/Models/WebsiteVisitLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Website\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WebsiteVisitLog extends Model
+{
+    use HasFactory;
+}
