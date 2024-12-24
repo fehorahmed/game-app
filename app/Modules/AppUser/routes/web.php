14f66a945de6e98f0a59bd76d105bfdd7273441e
fehor-sebpo <fehor.ahmed@sebpo.com>
2024-12-24 17:57:26 +0600

    ok

--- app/Modules/AppUser/routes/web.php
+++ app/Modules/AppUser/routes/web.php
@@ -67,6 +67,7 @@ Route::middleware(['auth:appuser'])->group(function () {
     Route::get('/website_visit_count/{website}', [AppUserController::class, 'appUserWebsiteVisitCount'])->name('user.website_visit_count');
 
     Route::get('/web_visiting_list', [AppUserController::class, 'appUserWebVisitingList'])->name('user.web_visiting_list');
+    Route::get('/web_routing_visit_count/{website}', [AppUserController::class, 'appUserWebsiteVisitCount'])->name('user.website_visit_count');
 
     Route::get('/star_buy', [StarLogController::class, 'userStarBuy'])->name('user.star.buy');
     Route::get('/get_user_by_id_for_add_yourself', [AppUserController::class, 'getUserByUserIdForAddYourself'])->name('user.get-user-by-user_id-for-add-yourself');
