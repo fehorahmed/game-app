b745f6471c0de30fa046ac07578b8bb5773cde58
ahmedfehor <ahmedfehor@gmail.com>
2025-01-13 11:05:07 +0600

    ok

--- app/Http/Resources/PaymentMethodResource.php
+++ app/Http/Resources/PaymentMethodResource.php
@@ -14,6 +14,21 @@ class PaymentMethodResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "account_no" => $this->account_no,
+            "account_type" => $this->account_type,
+            "manual_text" => $this->manual_text,
+            "limit_start" => $this->limit_start,
+            "limit_end" => $this->limit_end,
+            "withdraw_limit_start" => $this->withdraw_limit_start,
+            "withdraw_limit_end" => $this->withdraw_limit_end,
+            "transaction_fee" => $this->transaction_fee,
+            "logo" => $this->logo ? asset($this->logo) : null,
+            "status" => $this->status,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+        ];
     }
 }
