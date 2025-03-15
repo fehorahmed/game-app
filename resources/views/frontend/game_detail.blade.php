40aa2b5a1b15be360b521c714be387c7f5079eb9
ahmedfehor <ahmedfehor@gmail.com>
2025-03-15 17:03:42 +0600

    link added

--- resources/views/frontend/game_detail.blade.php
+++ resources/views/frontend/game_detail.blade.php
@@ -23,6 +23,22 @@
                     @endif
 
                 </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        @if ($game->google_drive_url || $game->microsoft_drive_url)
+                            <h4 class="text-center mt-3">Download links</h4>
+
+                            @if ($game->google_drive_url)
+                                <a href="{{ $game->google_drive_url }}" class="btn btn-primary"><i class="fa fa-download" aria-hidden="true"></i> Google Drive</a>
+                            @endif
+                            @if ($game->microsoft_drive_url)
+                                <a class="btn btn-primary" href="{{ $game->microsoft_drive_url }}"><i class="fa fa-download" aria-hidden="true"></i> Microsoft Drive</a>
+                            @endif
+                        @endif
+                    </div>
+
+
+                </div>
 
             </div>
         </div>
